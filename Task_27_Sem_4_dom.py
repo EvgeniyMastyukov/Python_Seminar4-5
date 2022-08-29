775d28ba22bd9ff3c8484e8d4d03ec84a344e991
Evgeniy <e.mastyukov@mail.ru>
2022-08-29 16:59:06 +0300

    add changes to task_27_dom

--- Task_27_Sem_4_dom.py
+++ Task_27_Sem_4_dom.py
@@ -2,9 +2,8 @@
 # В качестве символа-разделителя используйте пробел.
 
 text = '2 1 4 5 5 3 0 7 9 1'
-new_text = text.split(' ')
-for i in range(len(new_text)):
-    new_text[i] = int(new_text[i])
+new_text = list(map(int,text.split(' ')))
+
 print(new_text)
 print(f'min number = {min(new_text)}')  
 print(f'max number = {max(new_text)}') 
